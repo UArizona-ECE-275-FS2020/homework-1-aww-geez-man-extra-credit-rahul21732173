d78c8763624889f9839cbc4b2febb7e2f6a81e36
rahul21732173 <70229997+rahul21732173@users.noreply.github.com>
2020-09-29 09:21:37 +0530

    Update Morty.hpp

--- src/Morty.hpp
+++ src/Morty.hpp
@@ -1,21 +1,14 @@
-/* 
- __  __            _               _                 
-|  \/  | ___  _ __| |_ _   _      | |__  _ __  _ __  
-| |\/| |/ _ \| '__| __| | | |     | '_ \| '_ \| '_ \ 
-| |  | | (_) | |  | |_| |_| |  _  | | | | |_) | |_) |
-|_|  |_|\___/|_|   \__|\__, | (_) |_| |_| .__/| .__/ 
-                       |___/            |_|   |_|    
-*/ 
-
 #include <string> 
 #include <iostream>
 
+using namespace std;
 
-/* Extra credit below:
-namespace interdimensional { 
+
+
+namespace interdimensional {
 	// Declare enumeration to hold the dimensions each Morty is from
-	enum dimension {UNKNOWN = -1, C137, Z286};
-	
+	enum dimension { UNKNOWN = -1, C137, Z286 };
+
 	// Declare a structure to hold the arguments passed to the function
 	struct args {
 		int start;
@@ -23,19 +16,30 @@ namespace interdimensional {
 		int step;
 		dimension dim;
 	};
-	
 	// Declare a function to parse the args and return the above declared structure
 	args parseArgs(int c, char** v);
-	
+
 }
-*/
-
-// =========================================================================== // 
-//          Declare Namespace C137 and enclosed functions here 
-// =========================================================================== // 
 
 
+//C137 name space
+namespace C137 {
 
-// =========================================================================== // 
-//          Declare Namespace Z286 and enclosed functions here 
-// =========================================================================== // 
+    void Morty(int start, int stop, int step);
+   
+    void Morty(int start, int stop);
+        
+    
+
+}
+
+// Z286 name space
+namespace Z286 {
+
+    void Morty(int start, int stop, int step);
+
+    void Morty(int start, int stop);
+}
+
+
+
